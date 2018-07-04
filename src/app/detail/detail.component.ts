909ba3f8be63e5f913da622d0361d5e430e04b8b
hschne <hans.schnedlitz@gmail.com>
2018-07-04 17:10:11 +0200

    Add submitting with reactive form

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {KittenService} from '../kitten.service';
 import {Kitten} from '../kitten';
@@ -10,7 +10,7 @@ import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.scss']
 })
-export class DetailComponent implements OnInit {
+export class DetailComponent implements OnInit, OnChanges {
 
   kitten: Kitten;
 
@@ -25,20 +25,44 @@ export class DetailComponent implements OnInit {
   ngOnInit() {
     this.getKitten();
     this.kittenForm = this.formBuilder.group({
-      name: [this.kitten.name, Validators.required],
-      speed: [this.kitten.speed, [Validators.max(100), Validators.min(0)]],
-      strength: this.kitten.strength,
-      cuteness: this.kitten.cuteness
+      name: ['', Validators.required],
+      speed: ['', [Validators.required, Validators.max(100), Validators.min(0)]],
+      strength: ['', [Validators.required, Validators.max(100), Validators.min(0)]],
+      cuteness: ['', [Validators.required, Validators.max(100), Validators.min(0)]]
     });
+    this.rebuildForm();
+
+    this.kittenForm.valueChanges
+      .subscribe(val => {
+        if (this.kittenForm.valid) {
+          this.kitten.battlescore = val.speed + val.strength + val.cuteness;
+          console.log('TEST');
+        }
+      });
   }
 
-  get name() {
+  ngOnChanges(changes: SimpleChanges): void {
+    this.rebuildForm();
+  }
+
+  get kittenName() {
     return this.kittenForm.get('name');
   }
 
+  get speed() {
+    return this.kittenForm.get('speed');
+  }
+
+  get strength() {
+    return this.kittenForm.get('strength');
+  }
+
+  get cuteness() {
+    return this.kittenForm.get('cuteness');
+  }
 
   onSubmit() {
-    this.kittenService.update(this.kitten);
+    this.kittenService.update(this.prepareSaveKitten());
   }
 
   getKitten(): void {
@@ -48,5 +72,33 @@ export class DetailComponent implements OnInit {
 
   goBack(): void {
     this.location.back();
+  }
+
+  private prepareSaveKitten(): Kitten {
+    const formModel = this.kittenForm.value;
+
+    // return new `Hero` object containing a combination of original hero value(s)
+    // and deep copies of changed form model values
+    return {
+      id: this.kitten.id,
+      name: formModel.name as string,
+      speed: formModel.speed as number,
+      strength: formModel.strength as number,
+      cuteness: formModel.cuteness as number,
+      battlescore: this.kitten.battlescore
+    };
+  }
+
+  private rebuildForm() {
+    this.kittenForm.reset({
+      name: this.kitten.name,
+      speed: this.kitten.speed,
+      strength: this.kitten.strength,
+      cuteness: this.kitten.cuteness,
+    });
+  }
+
+  onChanges(): void {
+
   }
 }
