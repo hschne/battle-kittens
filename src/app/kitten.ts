ffb9435ac716df0f3ca93013afa7829d6a1ae241
hschne <hans.schnedlitz@gmail.com>
2018-07-22 19:36:10 +0200

    Improve testdata

--- src/app/kitten.ts
+++ src/app/kitten.ts
@@ -12,11 +12,11 @@ export const KITTENS: Kitten[] = [
   {id: 2, name: 'Anders', speed: 56, strength: 83, cuteness: 31},
   {id: 3, name: 'Bull', speed: 12, strength: 100, cuteness: 30},
   {id: 4, name: 'Alistair', speed: 64, strength: 29, cuteness: 91},
-  {id: 5, name: 'Kaidan', speed: 78, strength: 50, cuteness: 89},
-  {id: 6, name: 'Flemeth', speed: 78, strength: 50, cuteness: 89},
-  {id: 7, name: 'Miranda', speed: 78, strength: 50, cuteness: 89},
-  {id: 8, name: 'Wrex', speed: 78, strength: 50, cuteness: 89},
-  {id: 9, name: 'Hawke', speed: 78, strength: 50, cuteness: 89},
-  {id: 10, name: 'Leliana', speed: 78, strength: 50, cuteness: 89},
+  {id: 5, name: 'Kaidan', speed: 34, strength: 70, cuteness: 2},
+  {id: 6, name: 'Flemeth', speed: 55, strength: 21, cuteness: 9},
+  {id: 7, name: 'Miranda', speed: 60, strength: 61, cuteness: 72},
+  {id: 8, name: 'Wrex', speed: 20, strength: 96, cuteness: 43},
+  {id: 9, name: 'Hawke', speed: 30, strength: 42, cuteness: 88},
+  {id: 10, name: 'Leliana', speed: 78, strength: 15, cuteness: 32},
 ];
 
